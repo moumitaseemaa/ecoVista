82496e00d8633a0e768009eda05a9cef4dca0fed
Moumita Seema <moumitaseemaa@gmail.com>
2025-12-31 23:45:39 +0600

    suspense has added

--- app/page.js
+++ app/page.js
@@ -1,6 +1,24 @@
+// import LocationDetector from "@/components/LocationDetector";
+
+// export default function Home() {
+//   return <LocationDetector />;
+// }
 
 import LocationDetector from "@/components/LocationDetector";
+import { Suspense } from "react";
 
 export default function Home() {
-  return <LocationDetector />;
+  return (
+    <main>
+      <Suspense
+        fallback={
+          <div className="flex justify-center items-center h-screen bg-slate-700 text-white">
+            <p className="text-2xl">Loading Location Detector...</p>
+          </div>
+        }
+      >
+        <LocationDetector />
+      </Suspense>
+    </main>
+  );
 }
